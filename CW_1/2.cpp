3dc48581541dda73676b30895894ad74327e628b
MariaDeyneka <62387478+MariaDeyneka@users.noreply.github.com>
2020-03-27 11:29:24 +0200

    Update 2.cpp

--- CW_1/2.cpp
+++ CW_1/2.cpp
@@ -1,6 +1,16 @@
 #include <iostream>
-#include <string>
-using namespace std;
-string number:
-  cin>>number:
-  cout<<abs(stoi(number))<<endl;
+#include <cmath>
+
+using namespace std ;
+
+int main()
+{
+    
+    int *a = new int;
+    
+    cin >> *a;
+    *a = abs(*a);
+    cout << *a << endl;
+    
+    return 0;
+}
