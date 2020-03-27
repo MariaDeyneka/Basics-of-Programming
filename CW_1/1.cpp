bdcb1cd1dddd662b80e7f96e07cd3f1127f048de
MariaDeyneka <62387478+MariaDeyneka@users.noreply.github.com>
2020-03-27 11:31:41 +0200

    Update 1.cpp

--- CW_1/1.cpp
+++ CW_1/1.cpp
@@ -1,1 +1,16 @@
-
+#include <iostream>
+#include <cmath>
+using namespace std;
+int main()
+{
+  int n,m;
+  cin>>n;
+  cin>>m;
+  std::cout<<"n^m= "<< pow (n, m)<< std::endl;
+  do{
+    int sum=0;
+    sum=pow(n, 2)+pow(n, m);
+  }while(n==m);
+  cout<<"Сумма дорівнює "<< sum << endl;
+  return 0;
+}
